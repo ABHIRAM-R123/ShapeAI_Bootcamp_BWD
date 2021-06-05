fa651f8f2566f3ab83bbc8a904d979b0104e3fd6
ABHIRAM-R123 <85209287+ABHIRAM-R123@users.noreply.github.com>
2021-06-05 12:55:59 +0530

    Add files via upload

---
+++ Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+      <h1>ABHIRAM'S CLASSES !</h1>
+    </header>
+  );
+}
+
+export default Header;
---
+++ Note.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>AIM</h1>
+      <p>
+        STUDENT FROM NIT CALICUT, WITH THE AIM OF
+        PROVIDING SIMPLE AND FUN EDUCATION TO ALL.
+      </p>
+      <h1>YT Channel link : </h1>
+      <p>
+      https://www.youtube.com/channel/UCQxbrT4ejs6OnSsHqzwfT0A/videos
+      </p>
+    </div>
+  );
+}
+
+export default Note;
